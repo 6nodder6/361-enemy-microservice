be843799faefb44035c9c50f0e30eae56b0377c1
Ian Dugo <6nodder6@gmail.com>
2024-02-26 22:36:17 -0800

    Updated readme, added gitignore, commented compile.sh

--- compile.sh
+++ compile.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+# Compile the .o file
 echo g++ enemysharelib.cpp -c
 rm -f enemysharelib.o
 g++ enemysharelib.cpp -c
 
+# Compile the microservice
 echo g++ microprocess.cpp -o enemymicroprocess
 rm -f enemymicroprocess
 g++ microprocess.cpp -o enemymicroprocess
 
+# Example program compilation
 echo g++ example.cpp enemysharelib.o -o runexample
 rm -f runexample
 g++ example.cpp enemysharelib.o -o runexample
